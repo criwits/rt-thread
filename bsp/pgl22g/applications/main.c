d384cf2c22b265b7b93565b5053bfff33c7c4028
criwits <hanswan@tom.com>
2022-10-26 12:06:37 +0800

    Add Coremark

--- bsp/pgl22g/applications/main.c
+++ bsp/pgl22g/applications/main.c
@@ -13,48 +13,9 @@
 #include <stdio.h>
 #include <string.h>
 
-#include <rtthread.h>
 
-/* 定时器的控制块 */
-static rt_timer_t timer1;
-static rt_timer_t timer2;
-static int cnt = 0;
-
-/* 定时器 1 超时函数 */
-static void timeout1(void *parameter)
-{
-    rt_kprintf("periodic timer is timeout %d\n", cnt);
-
-    /* 运行第 10 次，停止周期定时器 */
-    if (cnt++>= 9)
-    {
-        rt_timer_stop(timer1);
-        rt_kprintf("periodic timer was stopped! \n");
-    }
-}
-
-/* 定时器 2 超时函数 */
-static void timeout2(void *parameter)
-{
-    rt_kprintf("one shot timer is timeout\n");
-}
-
-int main(void)
-{
-    /* 创建定时器 1  周期定时器 */
-    timer1 = rt_timer_create("timer1", timeout1,
-                             RT_NULL, 10,
-                             RT_TIMER_FLAG_PERIODIC);
-
-    /* 启动定时器 1 */
-    if (timer1 != RT_NULL) rt_timer_start(timer1);
-
-    /* 创建定时器 2 单次定时器 */
-    timer2 = rt_timer_create("timer2", timeout2,
-                             RT_NULL,  30,
-                             RT_TIMER_FLAG_ONE_SHOT);
-
-    /* 启动定时器 2 */
-    if (timer2 != RT_NULL) rt_timer_start(timer2);
+int main(void) {
+    rt_kprintf("RT-Thread on PGL22G RV32 IP Core!\n");
+    rt_kprintf("Long Live CHIRO! In CHIRO We Trust!\n");
     return 0;
 }
