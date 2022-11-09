6901ddc799375999bfb19e9ddbb294f659101858
Chiro Liang <Chiro2001@163.com>
2022-11-09 11:12:27 +0800

    update

--- bsp/pgl22g/applications/main.c
+++ bsp/pgl22g/applications/main.c
@@ -14,14 +14,66 @@
 #include <string.h>
 
 
+
+#define THREAD_STACK_SIZE   1024
+#define THREAD_PRIORITY     20
+#define THREAD_TIMESLICE    10
+
+/* 针对每个线程的计数器 */
+volatile rt_uint32_t count[2];
+
+/* 线程 1、2 共用一个入口，但入口参数不同 */
+static void thread_entry(void* parameter)
+{
+    rt_uint32_t value;
+
+    value = (rt_uint32_t)parameter;
+    while (1)
+    {
+        rt_kprintf("thread %d is running\n", value);
+        rt_thread_mdelay(1000); // 延时一段时间
+    }
+}
+
+static rt_thread_t tid1 = RT_NULL;
+static rt_thread_t tid2 = RT_NULL;
+
+static void hook_of_scheduler(struct rt_thread* from, struct rt_thread* to)
+{
+    rt_kprintf("from: %s -->  to: %s \n", from->name , to->name);
+}
+
+int scheduler_hook(void)
+{
+    /* 设置调度器钩子 */
+    rt_scheduler_sethook(hook_of_scheduler);
+
+    /* 创建线程 1 */
+    tid1 = rt_thread_create("thread1",
+                            thread_entry, (void*)1,
+                            THREAD_STACK_SIZE,
+                            THREAD_PRIORITY, THREAD_TIMESLICE);
+    if (tid1 != RT_NULL)
+        rt_thread_startup(tid1);
+
+    /* 创建线程 2 */
+    tid2 = rt_thread_create("thread2",
+                            thread_entry, (void*)2,
+                            THREAD_STACK_SIZE,
+                            THREAD_PRIORITY,THREAD_TIMESLICE - 5);
+    if (tid2 != RT_NULL)
+        rt_thread_startup(tid2);
+    return 0;
+}
+
 int main(void) {
-    rt_kprintf("RT-Thread on PGL22G RV32 IP Core!\n");
-    rt_kprintf("Long Live CHIRO! In CHIRO We Trust!\n");
+    rt_kprintf("RT-Thread on PGL22G RV32IMA Core!\n");
 #ifdef COREMARK_AUTORUN
     rt_kprintf("LAUNCHING Coremark...\n");
     int coremark_main(void);
     coremark_main();
     rt_kprintf("Coremark finished\n");
 #endif
+    scheduler_hook();
     return 0;
 }
